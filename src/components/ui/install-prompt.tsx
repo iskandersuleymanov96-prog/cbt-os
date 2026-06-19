06a9fc61a430d528e648627a5f07a5cf47c09378
Crystalheartme <crystalheartme@MacBook-Air-2.local>
2026-06-20 02:12:09 +0500

    fix: install-prompt TypeScript error

--- src/components/ui/install-prompt.tsx
+++ src/components/ui/install-prompt.tsx
@@ -20,7 +20,7 @@ export function InstallPrompt() {
 
     const handler = (e: Event) => {
       e.preventDefault()
-      setDeferredPrompt(e)
+      setDeferredPrompt(e as BeforeInstallPromptEvent)
       setVisible(true)
     }
 
